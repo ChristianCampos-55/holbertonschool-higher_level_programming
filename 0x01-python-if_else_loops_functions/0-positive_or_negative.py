a50496873e8b7e3e3b4d1bd67bd41a25019aed3d
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-28 17:17:32 +0000

    [FIX] Leftover code

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10, 10)
 if number == 0:
     print(number, 'is zero')
-elif number < 10 and number > 0:
+elif number > 0:
     print(number, 'is positive')
 else:
     print(number, 'is negaive')
