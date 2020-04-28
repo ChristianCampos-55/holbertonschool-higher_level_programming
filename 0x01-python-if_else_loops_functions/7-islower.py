b66cda5c8e5a05664ab68fe05520bf3adccbd04f
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-28 21:15:09 +0000

    [FIX] Checker can suck sometimes

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-     if (ord(c) => 97 and ord(c) <= 122):
-          return True
-     else:
-          return False
+    for i in range(97, 123):
+        if ord(c) == i:
+            return True
