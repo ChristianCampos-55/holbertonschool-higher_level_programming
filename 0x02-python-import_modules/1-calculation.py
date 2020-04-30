5f3e4142357e3d235f861832982123c1bbea1142
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-30 19:41:07 +0000

    [ADD] range in structure for a kudos life

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,8 @@ opn = [add, sub, mul, div]
 opr = ['+', '-', '*', '/']
 for i in range(0, 4):
     print('{} {} {} = {}'.format(a, opr[i], b, opn[i](a, b)))
-if__name__ == "__main__":
+
+if __name__ == '__main__':
     add(a, b)
     sub(a, b)
     mul(a, b)
