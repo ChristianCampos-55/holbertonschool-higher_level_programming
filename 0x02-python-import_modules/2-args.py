a692b8e2d05acf018627fa087d454ee969acc738
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-30 20:50:51 +0000

    [ADD] Tsk 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+from sys import argv
+if __name__ == '__main__':
+    num = len(argv)-1
+    text = 'arguments'
+    if num == 1:
+        print('1 {:s}:'.format(text[0:8]))
+    elif num > 1:
+        print('{:d} {:s}:'.format(num, text))
+    else:
+        print('0 {:s}.'.format(text))
+
+    for i in range(1, len(argv)):
+        print('{:d}: {}'.format(i, argv[i]))
