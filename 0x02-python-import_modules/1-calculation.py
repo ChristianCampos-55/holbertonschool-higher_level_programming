1a526be9922458d2f7f9b8bb2c58adbb9d35841e
ChristianCampos-55 <1566@holbertonschool.com>
2020-04-30 16:50:52 +0000

    [ADD] Tsk 1 with kudos

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+opn = [add, sub, mul, div]
+opr = ['+', '-', '*', '/']
+for i in range(0, 4):
+    print('{} {} {} = {}'.format(a, opr[i], b, opn[i](a, b)))
