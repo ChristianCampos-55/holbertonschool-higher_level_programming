236cd461783a584cf4b003ab2f64396bd599b464
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-06 20:35:14 +0000

    [FIX] Sometimes brute-forcing a print is the solution with the checker

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if 
     for x in matrix:
-        for y in x:
-            if y != x[-1]:
-                print('{:d}'.format(y), end='')
-            else:
-                print('{:d}'.format(x[-1]))
+        space = ""
+        for i in x:
+            print("{:s}{:d}".format(space, i), end="")
+            space = " "
+        print()
