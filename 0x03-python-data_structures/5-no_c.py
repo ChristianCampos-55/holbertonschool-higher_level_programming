f3633dd82c43fb78ed533f6b6f9ee74eae1e6704
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-06 19:17:10 +0000

    [FIX] Code without replace() function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string.replace('C', '')
-    return new_string.replace('c',  '')
+    new_str = ''
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            new_str += x
+    return new_str
