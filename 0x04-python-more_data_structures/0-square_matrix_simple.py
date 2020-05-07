7e431a3ffe63a7921988eaedbee8ea89fdd8aceb
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-07 22:31:31 +0000

    [FIX] Created if for the existence of matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    for x in matrix:
-        new_matrix.append(list(map(lambda y : y**2, x)))
+    if matrix:
+        for x in matrix:
+            new_matrix.append(list(map(lambda y : y**2, x)))
     return new_matrix
