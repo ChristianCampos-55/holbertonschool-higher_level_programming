c2f3920daefb63484a922f9603ff308e9901b959
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-20 02:11:08 +0000

    [FIX] Dumb identation error

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,12 +4,12 @@
 
 class Square:
     """ method class initialized AND exceptions raised for edge cases """
-    def __init__(self, size=0)
-    if type(size) != int:
-        raise TypeError('size must be an integer')
-    elif size < 0:
-        raise ValueError('size must be >= 0')
-    self.__size = size
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
 
     """ method for area of Square """
     def area(self):
