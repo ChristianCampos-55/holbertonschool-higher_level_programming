1a3b80c5ee953e1eb7c216f417e7139e15aba593
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-20 01:56:38 +0000

    [ADD] TSK 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Class Square with a size attribute """
+
+
 class Square:
     """ Class method initialization"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         self.__size = size
