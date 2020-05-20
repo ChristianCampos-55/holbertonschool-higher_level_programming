135d6fbc2d5adbc406495552edfe1669b5ef2869
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-20 04:10:45 +0000

    [ADD] TSK 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Class Square """
+
+class Square:
+    """ definition of size method, with excepted edge cases """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    """ set size getter """
+    @property
+    def size(self):
+        return self.__size
+
+    """ set size setter (yeah, try saying that three times) """
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    """ method to get area of Square """
+    def area(self):
+        return self.__size ** 2
+
+    """ method to print Square size * length with '#' """
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for i in range(self.__size):
+                print('#', end='')
+            print()
