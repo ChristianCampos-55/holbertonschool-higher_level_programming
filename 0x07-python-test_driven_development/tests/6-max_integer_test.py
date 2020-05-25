21d306c9573ea55aa62d2eaff7286157f4b73b87
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-25 01:09:33 +0000

    [ADD] TSK 6 unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,22 +14,22 @@ class TestMaxInteger(unittest.TestCase):
         tlist = [3, 2, 1]
         self.assertEqual(max_integer(tlist), 3)
 
-    def test_start(self):
+    def test_middle(self):
         tlist = [2, 3, 1]
         self.assertEqual(max_integer(tlist), 3)
 
-    def test_start(self):
+    def test_negative(self):
         tlist = [-3, 2, 1]
         self.assertEqual(max_integer(tlist), 2)
 
-    def test_start(self):
+    def test_negative_all(self):
         tlist = [-3, -2, -1]
         self.assertEqual(max_integer(tlist), -1)
 
-    def test_start(self):
+    def test_one(self):
         tlist = [1]
         self.assertEqual(max_integer(tlist), 1)
 
-    def test_start(self):
+    def test_none(self):
         tlist = []
         self.assertEqual(max_integer(tlist), None)
