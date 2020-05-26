ebbf4ca759bc1ae3bd21c4367f5454bb18fa16b5
ChristianCampos-55 <1566@holbertonschool.com>
2020-05-26 00:58:43 +0000

    [ADD] TSK 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,23 +14,23 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, width):
-        if type(self.__width) is not int:
+    def width(self, value):
+        if type(value) is not int:
             raise TypeError('width must be an integer')
-        elif self.__width < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         else:
-            self.__width = width
+            self.__width = value
 
     @property
     def height(self):
         return self.__height
 
     @height.setter
-    def height(self, height):
-        if type(self.__height) is not int:
+    def height(self, value):
+        if type(value) is not int:
             raise TypeError('width must be an integer')
-        elif self.__height < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         else:
-            self.__height = height
+            self.__height = value
