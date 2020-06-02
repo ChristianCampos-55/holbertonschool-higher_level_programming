444bae4b29ad9fa088fe09d00163af97409937ba
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-02 23:06:36 +0000

    [FIX] Changed syntax of return value

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,5 +6,4 @@ def inherits_from(obj, a_class):
     """ Function that returns whether an obj inherits, strictly,
         from a class """
 
-    if type(obj) != a_class and isinstance(obj, a_class):
-        return True
+    return type(obj) is not a_class and isinstance(obj, a_class)
