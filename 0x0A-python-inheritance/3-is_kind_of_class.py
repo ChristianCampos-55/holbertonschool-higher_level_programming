84244d1c24c9360e65b3c6fc59e57099bdf1727a
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-02 23:01:45 +0000

    [UPDT] Documentation

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+""" Module that hosts function is_kind_of_class """
+
+
 def is_kind_of_class(obj, a_class):
+    """ Returns whether obj is instance of a_class """
+
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+""" Module that hosts inherits_from function """
+
+
 def inherits_from(obj, a_class):
+    """ Function that returns whether an obj inherits, strictly,
+        from a class """
+
     if type(obj) != a_class and isinstance(obj, a_class):
         return True
