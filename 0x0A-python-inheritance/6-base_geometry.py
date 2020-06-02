64d72bf681ff189e0333b90f1b24cfddf5596411
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-02 23:26:53 +0000

    [ADD] TSK 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module that hosts the BaseGeometry class """
+
+
+class BaseGeometry:
+    """ area function to raise exception """
+
+    def area(self):
+        raise Exception('area() is not implemented')
