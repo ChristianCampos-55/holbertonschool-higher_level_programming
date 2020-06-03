699a9ce8825a226db31562b816d83c61fa9ed384
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-03 02:27:24 +0000

    [ADD] TSK 11

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Method that hosts MyInt class, which inherits from int and swaps
+    == and != operators """
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__qe__(other)
