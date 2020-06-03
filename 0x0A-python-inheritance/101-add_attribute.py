4107d76fe2f3f968d1a96372df617c6e02ef6939
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-03 03:05:11 +0000

    [FIX] Dumb typo, dic for dict

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,7 +2,8 @@
 """ Module that hosts function that adds an atrribute, if it can """
 
 
-def add_attribute(obj, name, val):
-    if not hasattr(obj, '__dic__'):
+def add_attribute(instance, name, value):
+    if hasattr(instance, '__dict__'):
+        setattr(instance, name, value)
+    else:
         raise TypeError("can't add new attribute")
-    setattr(obj, name, val)
