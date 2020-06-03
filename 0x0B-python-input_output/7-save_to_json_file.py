0d0718ee8ad275204ca2c1b37c4fa5c609f4649a
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-03 23:18:58 +0000

    [ADD] TSK 7

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" File that hosts the save_to_json_file function """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Function that writes and object to a text file using
+        json representation """
+    with open(filename, mode='w') as _file:
+        file.write(json.dumps(my_obj))
