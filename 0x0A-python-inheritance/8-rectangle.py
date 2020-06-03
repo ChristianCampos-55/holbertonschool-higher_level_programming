aba1cabefddf633e17d833e69d0128c77a9a611e
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-03 01:35:16 +0000

    [ADD] TSK 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module that hosts class Rectangle """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+
+class Rectangle(BaseGeometry):
+    """ Module that validates width and height values by inheriting
+        from BaseGeometry class """
+
+
+    def __init__(self, width, height):
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
