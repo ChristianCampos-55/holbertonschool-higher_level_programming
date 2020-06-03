02e42cdfd8fecc536278ec2d5b2abcf28e790208
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-03 02:18:17 +0000

    [FIX] Changed Rectangle for Square in return

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,3 +10,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.integer_validator('size', size)
         super().__init__(size, size)
+
+    def __str__(self):
+        return '[Square] {}/{}'.format(self.__size, self.__size)
