2fd01beba8ef2fc4b9f89f0ea52f7dffd9c94bed
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-05 00:02:43 +0000

    [ADD] TSK 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+
+/**
+ * check_cycle - function that finds if a linked list is looped
+ * @list: pointer to head of list
+ * Return: 0 if looped, 1 otherwise
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *check_one = list, *check_two = list;
+
+	while (check_one && check_two && list)
+	{
+		check_one = check_one->next;
+		check_two = check_two->next->next;
+
+		if (check_one == check_two)
+			return (1);
+	}
+
+	return (0);
+}
