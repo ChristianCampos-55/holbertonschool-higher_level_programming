a5f4611e8379aca9f810fd378afd07b54fd939f1
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-09 21:38:12 +0000

    [ADD] TSK 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,28 @@
 #!/usr/bin/python3
-'''base file'''
-import json
+""" Module that hosts the class Base """
 
 
 class Base:
-    '''Base Class'''
+    """ Simple Base class """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        '''Base __init__'''
-        if id:
+
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = self.__nb_objects
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries is []:
+            return '[]'
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            return json.loads(list_objs)
