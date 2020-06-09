a5a34c85a6d3cc5acdb5c1df181a9b1079c721b4
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-09 19:24:13 +0000

    [ADD] Rects from scratch

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,16 +3,15 @@
 from models.base import Base
 
 
-
 class Rectangle(Base):
     """ Class Rectangle which inherits from base """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
     def area(self):
         return self.width * self.height
