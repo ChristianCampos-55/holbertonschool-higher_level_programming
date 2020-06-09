75c0cad74ddb76727725db5f7ecb9b8b1c1af664
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-09 21:35:43 +0000

    [ADD] TUNIS

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-''' Module that hosts the class Base '''
+'''base file'''
+import json
 
 
 class Base:
-    ''' Simple Base class '''
-
+    '''Base Class'''
     __nb_objects = 0
 
     def __init__(self, id=None):
-
-        if id is not None:
+        '''Base __init__'''
+        if id:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        if list_dictionaries is None or list_dictionaries is []:
-            return '[]'
-        else:
-            return json.dumps(list_dictionaries)
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        if list_objs is None:
-            return json.loads(list_objs)
+            self.id = self.__nb_objects
