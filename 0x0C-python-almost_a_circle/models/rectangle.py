dbcbe811064dac6838d878d6d5558dbf693473b1
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-09 20:48:39 +0000

    [ADD] Rect

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,11 +8,11 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Rec's constructor with super ID (non-psychology)"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
@@ -20,13 +20,13 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, val):
         """ setter width """
-        if type(width) is not int:
+        if type(val) is not int:
             raise TypeError('width must be an integer')
-        if width <= 0:
+        if val <= 0:
             raise ValueError('width must be > 0')
-        self.__width = width
+        self.__width = val
 
     @property
     def height(self):
@@ -34,13 +34,13 @@ class Rectangle(Base):
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, val):
         """ setter height """
-        if type(height) is not int:
+        if type(val) is not int:
             raise TypeError('height must be an integer')
-        if height <= 0:
+        if val <= 0:
             raise ValueError('height must be > 0')
-        self.__height = height
+        self.__height = val
 
     @property
     def x(self):
@@ -48,13 +48,13 @@ class Rectangle(Base):
         return self.__x
 
     @x.setter
-    def x(self, x):
+    def x(self, val):
         """ setter x """
-        if type(x) is not int:
+        if type(val) is not int:
             raise TypeError('x must be an integer')
-        if x < 0:
+        if val < 0:
             raise ValueError('x must be >= 0')
-        self.__x = x
+        self.__x = val
 
     @property
     def y(self):
@@ -62,10 +62,10 @@ class Rectangle(Base):
         return self.__y
 
     @y.setter
-    def y(self, y):
+    def y(self, val):
         """ setter y """
-        if type(y) is not int:
+        if type(val) is not int:
             raise TypeError('y must be an integer')
-        if x < 0:
+        if val < 0:
             raise ValueError('y must be >= 0')
-        self.__y = y
+        self.__y = val
