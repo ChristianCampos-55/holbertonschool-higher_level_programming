6e3719bffb23fc6e5a53d31a1e5c27e591f1fd04
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-09 22:28:28 +0000

    [ADD] Rect

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,8 +5,21 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ Class sqaure which inherits from rectangle """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """ class constructor """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ getter for size with Rectangle's width """
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        """ setter for size with Rectangle's width """
+        self.width = val
+        self.height = val
 
     def __str__(self):
         """ re-resignification of string method """
