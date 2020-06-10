a857694d1faaa059fd780075959727b77db53512
ChristianCampos-55 <1566@holbertonschool.com>
2020-06-10 00:31:25 +0000

    [ADD] TSK 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -33,8 +33,9 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """ public method that returns a dict representation of the class """
-        return {'id': self.id, 'width': self.width, 'height': self.height,
+        dic = {'id': self.id, 'width': self.width, 'height': self.height,
                 'x': self.x, 'y':self.y}
+        return dic
 
     def update(self, *args, **kwargs):
         """ method to update values with args """
