02117985515cfa0172456b796702e6fce3c90fef
ChristianCampos-55 <1566@holbertonschool.com>
2020-07-14 19:36:27 +0000

    [FIX] Typo

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,4 +2,7 @@
 -- creates a new 'second_table' and adds new rows
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES
-(1, "John", 10"), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
+    (1, "John", 10"),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
