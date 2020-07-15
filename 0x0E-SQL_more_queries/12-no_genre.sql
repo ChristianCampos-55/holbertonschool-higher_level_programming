e02fb50ebc0edf4a886d09238840dae03b7b3f94
ChristianCampos-55 <1566@holbertonschool.com>
2020-07-15 01:58:09 +0000

    [FIX] Typo

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows in 'hbtn_0d_tvshows'
 -- shows must not have a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id is NULL ORDER BY tv_shows.title;
