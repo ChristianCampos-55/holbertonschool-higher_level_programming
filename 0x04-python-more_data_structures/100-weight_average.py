66d78e9a69f58b0737508a35cc67d95963eeb62a
ChristianCampos-55 <1566@holbertonschool.com>
2020-11-27 23:54:32 +0000

    [ADD] ADV TSK 1

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+        n, d = 0, 0
+        for i in my_list:
+            n += i[0] * i[1]
+            d += i[1]
+        return n / d
