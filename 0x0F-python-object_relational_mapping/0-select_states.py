aa57ef695c287cd651d1c88e836fa40c755f062c
ChristianCampos-55 <1566@holbertonschool.com>
2020-12-12 19:39:02 +0000

    [FIX]Typo

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-'''
-list staates in database
-'''
+"""list staates in database"""
 if __name__ == '__main__':
     import sys
     import MySQLdb
 
     db = MySQLdb.connect(user=sys.argv[1], pass=sys.argv[2],
-                               db=sys.argv[3], host='localhost()',
-                               port=3306)
+                               db=sys.argv[3], port=3306, host='localhost()')
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states ORDER BY states.id ASC;')
     states = cursor.fetchall()
     for st in states:
-        print(state)
+        print(st)
     cursor.close()
     db.close()
