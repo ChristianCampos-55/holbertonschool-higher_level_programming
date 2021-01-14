c165990e2630f645cb0b5c936210a2c59904ca38
Dave <1566@holbertonschool.com>
2021-01-14 14:29:45 -0500

    [ADD]TSK 3 (Giving me [Errno 111] on my PC, but have a hunch will work on Holb's servers)

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a Request to a URL and displays the decoded response"""
+from urllib import request, error
+from sys import argv
+
+
+if __name__ == '__main__':
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)
