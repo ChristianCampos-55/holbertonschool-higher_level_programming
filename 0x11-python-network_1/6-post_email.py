5e808721101d7e56043c9d0b767e525b32ef4333
Dave <1566@holbertonschool.com>
2021-01-14 16:58:07 -0500

    [FIX]Replaced from sys argv to just import sys

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Takes in a URL and Email, sends req to URL with mail as param, shows response"""
-from sys import argv
+import sys
 import requests
 
 if __name__ == '__main__':
-    req = requests.post(argv[1], {'email': argv[2]})
+    req = requests.post(sys.argv[1], {'email': sys.argv[2]})
     print(req.text)
