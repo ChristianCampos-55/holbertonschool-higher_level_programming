7d4a62cd70fb5409de1a8c60da16583c276e98d0
Dave <1566@holbertonschool.com>
2021-01-14 18:00:38 -0500

    [FIX]alt letter not printing (apparently)

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,13 +6,13 @@ import sys
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
     letter = ''
-    if len(sys.argv) > 1:
+    if len(sys.argv) >= 1:
         letter = sys.argv[1]
     data = {'q': letter}
     requested = requests.post(url, data)
     try:
         js_on = requested.json()
-        if not js_on:
+        if js_on == {}:
             print('No result')
         else:
             print('[{}] {}'.format(result.get('id'), result.get('name')))
