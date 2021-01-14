677d1b4dbb58d82fa28983e792044c258cf9268b
Dave <1566@holbertonschool.com>
2021-01-14 18:14:31 -0500

    [ADD]TSK 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Displays the ID of GitHub with credentials passed as args"""
+import sys
+import requests
+
+if __name__ == '__main__':
+    to_print = requests.get('https://api.github.com/user', auth=(sys.argv[1], sys.argv[2]))
+    print(to_print.json().get('id'))
