68026cef8df2c62ffcbee95bb45fc746846bf2f7
Dave <1566@holbertonschool.com>
2021-01-14 17:18:22 -0500

    [ADD]TSK 7 & 8 (Errno 111 seems unbeatable - Checker is my debugger)

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Display response of requested URL"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    to_print = requests.get(sys.argv[1])
+    if to_print.status_code >= 400:
+        print('Error code:', to_print.status_code)
+    else:
+        print(toprint.text)
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Sends a POST request with a letter as parameter"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    data = {'q': ''}
+    url = "http://0.0.0.0:5000/search_user"
+    requested = requests.post(url, data)
+    if len(sys.argv) > 1:
+        user = sys.argv[1]
+    try:
+        js_on = req.json()
+        if not js_on:
+            print('No result')
+        else:
+            print('[{}] {}'.format(result.get('id'), result.get(name)))
+    except:
+        print('Not a valid JSON')
