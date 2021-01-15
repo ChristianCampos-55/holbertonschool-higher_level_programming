e41f12fb899e55181d8c44c300b2bb6dbac22224
Dave <1566@holbertonschool.com>
2021-01-15 15:38:49 -0500

    [FIX]Rewrote exec

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306, host='localhost')
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM states WHERE name\
-    LIKE BINARY name="{}"'.format(argv[4]))
+    cursor.execute('SELECT * FROM states WHERE\
+    BINARY name="{}"'.format(argv[4]))
 
     queried = cursor.fetchall()
     for i in queried:
