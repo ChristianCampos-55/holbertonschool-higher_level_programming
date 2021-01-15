d42271ac80500bac9b74797392719d84a027d571
Dave <1566@holbertonschool.com>
2021-01-15 12:19:10 -0500

    [FIX]Refactor

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""list staates in database"""
+"""list states in database"""
+from sys import argv
+import MySQLdb
+
 if __name__ == '__main__':
 
-    import sys
-    import MySQLdb
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                               db=sys.argv[3], port=3306, host='localhost()')
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2],
+                         db=argv[3], port=3306, host='localhost()')
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states ORDER BY id ASC;')
     states = cursor.fetchall()
     for st in states:
         print(st)
-    cursor.close()
-    db.close()
