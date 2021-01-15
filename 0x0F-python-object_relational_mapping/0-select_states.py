a20713441b1b7f7b390968a5e4e12d7d2f39c750
Dave <1566@holbertonschool.com>
2021-01-15 13:20:41 -0500

    [FIX]I'm just trying whatever and using checker as debigger cause of infinite port issues on local PC, I've also learned life and effort in an infinite timeline are basically meaningless

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import MySQLdb
 if __name__ == '__main__':
 
     db = MySQLdb.connect(user=argv[1], passwd=argv[2],
-                         db=argv[3], port=3306, host='localhost()')
+                         db=argv[3], port=3306, host='localhost')
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states ORDER BY id ASC;')
     states = cursor.fetchall()
