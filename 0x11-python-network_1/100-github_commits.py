495339b4301545b958a947d2a4cdb294548761df
Dave <1566@holbertonschool.com>
2021-01-14 22:31:08 -0500

    [FIX]Pep8 paradox

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
                 for i in range(0, 10):
                         print('{}: {}'.format(j_on[i].get('sha'), j_on[i].get(
                                 'commit').get('author').get('name')))
-        except ValueError:
+        except:
                 pass
