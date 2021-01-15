aa2969630ea7d670db7a5414d46a8c0829c195f5
Dave <1566@holbertonschool.com>
2021-01-14 22:29:48 -0500

    [FIX]Pep8

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,11 +6,11 @@ import sys
 
 if __name__ == '__main__':
         try:
-                to_get = requests.get('https://api.github.com/repos/{}/{}/commits'
-                                      .format(sys.argv[2], sys.argv[1]))
-                js_on = to_get.json()
+                get = requests.get('https://api.github.com/repos/{}/{}/commits'
+                                   .format(sys.argv[2], sys.argv[1]))
+                j_on = get.json()
                 for i in range(0, 10):
-                        print('{}: {}'.format(js_on[i].get('sha'), js_on[i].get('commit')
-                                              .get('author').get('name')))
-        except:
+                        print('{}: {}'.format(j_on[i].get('sha'), j_on[i].get(
+                                'commit').get('author').get('name')))
+        except ValueError:
                 pass
