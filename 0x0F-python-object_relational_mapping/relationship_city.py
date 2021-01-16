a573225d1cc89a65c6dda777fb159d359b2b506c
ChristianCampos-55 <1566@holbertonschool.com>
2021-01-16 13:10:11 -0500

    [ADD]Modules of Class City and State

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definition of class City"""
+from relationship_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+class City(Base):
+    '''Class City'''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement="auto")
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
